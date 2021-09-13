3e558c837bfe554ff108185872f54398a723fda1
michaeltetteh <miketetteh751@outlook.com>
2021-09-13 15:03:38 +0000

    sayhello service

---
+++ examples/sayhello_server.py
@@ -0,0 +1,36 @@
+from concurrent import futures
+import random
+import grpc
+
+
+from sayhello_pb2 import (RequestMsg, ResponseMsg)
+
+import sayhello_pb2_grpc
+
+
+class SayHelloService(sayhello_pb2_grpc.SayHelloServicer):
+
+    def SayHello(self, request, context):
+        i_d = request.user_id
+        return ResponseMsg(id=i_d,title="RECEIVED!")
+
+def serve():
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    sayhello_pb2_grpc.add_SayHelloServicer_to_server(
+
+        SayHelloService(), server
+
+    )
+
+    server.add_insecure_port("[::]:50051")
+
+    server.start()
+
+    server.wait_for_termination()
+
+
+
+if __name__ == "__main__":
+    serve()
