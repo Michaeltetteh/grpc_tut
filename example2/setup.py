e9237b80709aa4d15db462d412d6bad1aa32ae59
michaeltetteh <miketetteh751@outlook.com>
2021-09-14 11:26:50 +0000

    updated setup

--- example2/setup.py
+++ example2/setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='django_grpc',
     version='0.0.1',
+    description='client/server generated files',
+    author='Michael Tetteh',
+    author_email='test@test.test',
+    url='https://github.com/Michaeltetteh/grpc_tut',
     packages=find_packages(include=['django_grpc', 'django_grpc.*']),
     install_requires=[
       'grpcio==1.40.0'
