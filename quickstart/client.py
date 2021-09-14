bb8ca908b5842060c970ef280c77a58611eade76
michaeltetteh <miketetteh751@outlook.com>
2021-09-14 15:29:32 +0000

    client

--- quickstart/client.py
+++ quickstart/client.py
@@ -2,6 +2,7 @@ import grpc
 from quickstart_grpc.account_proto import account_pb2
 from quickstart_grpc.account_proto import account_pb2_grpc
 from google.protobuf.json_format import MessageToJson
+from quickstart_grpc.blog_proto import post_pb2, post_pb2_grpc
 
 
 with grpc.insecure_channel('localhost:50051') as channel:
@@ -19,4 +20,20 @@ with grpc.insecure_channel('localhost:50051') as channel:
     for user in stub.List(account_pb2.UserListRequest()):
         print(MessageToJson(user), end='')
 
-
+    post_stub = post_pb2_grpc.PostControllerStub(channel)
+    print('----- Create -----')
+    response = post_stub.Create(post_pb2.Post(title='t1', content='c1'))
+    print((MessageToJson(response)), end='')
+    print('----- List -----')
+    for post in post_stub.List(post_pb2.PostListRequest()):
+        print(MessageToJson(post), end='')
+    print('----- Retrieve -----')
+    response = post_stub.Retrieve(post_pb2.PostRetrieveRequest(id=response.id))
+    print(MessageToJson(response), end='')
+    print('----- Update -----')
+    response = post_stub.Update(post_pb2.Post(id=response.id, title='t2', content='c2'))
+    print(MessageToJson(response), end='')
+    
+    print('----- Delete -----')
+    post_stub.Destroy(post_pb2.Post(id=response.id))
+    
--- quickstart/quickstart/urls.py
+++ quickstart/quickstart/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from account.services import UserService
 from quickstart_grpc.account_proto import account_pb2_grpc
+# from quickstart_grpc.blog_proto import post_pb2_grpc
+# from blog.services import PostService
+from blog.handlers import grpc_handlers as blog_grpc_handlers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +13,4 @@ urlpatterns = [
 
 def grpc_handlers(server):
     account_pb2_grpc.add_UserControllerServicer_to_server(UserService.as_servicer(), server)
+    blog_grpc_handlers(server)
