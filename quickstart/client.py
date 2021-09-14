e3b1818c65e455b4d6f107ebf52e095a366fb6a8
michaeltetteh <miketetteh751@outlook.com>
2021-09-14 12:42:32 +0000

    client added

--- quickstart/client.py
+++ quickstart/client.py
@@ -5,5 +5,16 @@ from quickstart_grpc import account_pb2_grpc
 
 with grpc.insecure_channel('localhost:50051') as channel:
     stub = account_pb2_grpc.UserControllerStub(channel)
+    #retreive
+    print(stub.Retrieve(account_pb2.UserRetrieveRequest(id=1)))
+
+    #create
+    print(stub.Create(account_pb2.User(username="test_uasdsaaser",email="test@treassst.test")))
+    print(stub.Create(account_pb2.User(username="test_usdasear",email="test@tsreasst.test")))
+    #detroy
+    print(stub.Destroy(account_pb2.User(id=8)))
+
     for user in stub.List(account_pb2.UserListRequest()):
         print(user, end='')
+
+
