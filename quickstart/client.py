62ddf371516904f0267131291aaf799cc402e2bc
michaeltetteh <miketetteh751@outlook.com>
2021-09-14 12:59:04 +0000

    convert protobuf object to json

--- quickstart/client.py
+++ quickstart/client.py
@@ -1,20 +1,22 @@
 import grpc
 from quickstart_grpc import account_pb2
 from quickstart_grpc import account_pb2_grpc
+from google.protobuf.json_format import MessageToJson
 
 
 with grpc.insecure_channel('localhost:50051') as channel:
     stub = account_pb2_grpc.UserControllerStub(channel)
     #retreive
-    print(stub.Retrieve(account_pb2.UserRetrieveRequest(id=1)))
+    # print(stub.Retrieve(account_pb2.UserRetrieveRequest(id=1)))
+    print(MessageToJson(stub.Retrieve(account_pb2.UserRetrieveRequest(id=1))))
 
-    #create
-    print(stub.Create(account_pb2.User(username="test_uasdsaaser",email="test@treassst.test")))
-    print(stub.Create(account_pb2.User(username="test_usdasear",email="test@tsreasst.test")))
-    #detroy
-    print(stub.Destroy(account_pb2.User(id=8)))
+    # #create
+    # print(stub.Create(account_pb2.User(username="test_uasdsaaser",email="test@treassst.test")))
+    # print(stub.Create(account_pb2.User(username="test_usdasear",email="test@tsreasst.test")))
+    # #detroy
+    # print(stub.Destroy(account_pb2.User(id=8)))
 
     for user in stub.List(account_pb2.UserListRequest()):
-        print(user, end='')
+        print(MessageToJson(user), end='')
 
 
