5e63e7e02e74d3bea85a65f5553e482e8604dd89
michaeltetteh <miketetteh751@outlook.com>
2021-09-14 12:13:07 +0000

    quickstart project added

---
+++ quickstart/quickstart_grpc/__init__.py
@@ -0,0 +1,2 @@
+from .account_pb2 import *
+from .account_pb2_grpc import *
---
+++ quickstart/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quickstart_grpc',
+    version='0.0.1',
+    description='client/server generated files',
+    author='Michael Tetteh',
+    author_email='test@test.test',
+    url='https://github.com/Michaeltetteh/grpc_tut',
+    packages=find_packages(include=['quickstart_grpc', 'quickstart_grpc.*']),
+    install_requires=[
+      'grpcio==1.40.0'
+    ],
+)
